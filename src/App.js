8a5f6580573fd4388dacfea7f6d2f8a31a388f60
RubensRRCruz <rubens_cruz78@outlook.com>
2022-05-19 17:00:34 +0100

    criação de botões

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MyButton() {
+  return (
+    <button>Clica-me</button>
+  );
+}
+
 function App() {
   return (
-    
+    <div>
+      <h1>Bem vindo a minha aplicação</h1>
+      <MyButton/>
+      <MyButton/>
+      <MyButton/>
+    </div>
     
   );
 }
